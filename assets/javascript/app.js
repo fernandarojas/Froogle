2d0a75c04cd9ad727417bf0a1f4006b927957545
Furn <FernandaRojas@Fernandas-MacBook-Pro.local>
2018-07-01 20:13:56 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
 $("#find-item").on("click", function(event) { 
     event.preventDefault(); 
 
+    $("#wal-logo").empty();
     $("#walmart-items-view").empty();
     $("#ebay-items-view").empty();
-    
+
     wallmartApi();
     eBayApi();
 });
@@ -12,13 +13,19 @@ function wallmartApi() {
     var item = $("#item-input").val().trim();
     var queryURL = "http://api.walmartlabs.com/v1/search?apiKey=rq6dj88wfdksdssv8d2szvq8&query=" + item;
 
+   
+
+
     $.ajax({
         url: queryURL,
         dataType: "jsonp",
         type: "GET"
         }).then(function(response) {
             console.log(response);
-            
+
+            var walmartLogo = $("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Walmart_logo.svg/1280px-Walmart_logo.svg.png' id='wal-logo'>");
+            $("#wal-logo").append(walmartLogo);
+
             for (var i = 0; i < 10; i++) {
 
                 var itemDiv = $("<div class='card mx-auto text-center mb-4'>");
@@ -29,11 +36,14 @@ function wallmartApi() {
                 itemImage.attr("src", response.items[i].mediumImage);
 
                 var itemPrice = response.items[i].salePrice;
-                var p = $("<div class='card-footer text-center'>").text(itemPrice);
+                var p = $("<div class='card-footer text-center'>").text("$" + itemPrice);
+
+                var buyButton = $("<button type='button' class='buy-button btn btn-primary'>Buy Now</button>");
 
                 itemDiv.append(h);
                 itemDiv.append(itemImage);
                 itemDiv.append(p);
+                itemDiv.append(buyButton);
 
                 $("#wal-items-view").prepend(itemDiv);
             }
@@ -56,21 +66,28 @@ function eBayApi() {
         }).then(function(response) {
             console.log(response);
 
+            var ebayLogo = $("<img src=' https://upload.wikimedia.org/wikipedia/commons/4/48/EBay_logo.png' id='ebay-logo'>");
+            $("#ebay-logo").append(ebayLogo);
+           
+
             for (var i = 0; i < 10; i++) { 
             
                 var itemDiv = $("<div class='card mx-auto text-center mb-4'>");
-                var itemName = response.findItemsByKeywordsResponse[0].searchResult[0].item[i].title;
+                var itemName = response.findItemsByKeywordsResponse["0"].searchResult["0"].item[i].title;
                 var h = $("<div class='card-header'>").text(itemName);
 
                 var itemImage = $("<img class='card-text mx-auto itemImage'>");
-                itemImage.attr("src", response.findItemsByKeywordsResponse[0].searchResult[0].item[i].galleryURL);
+                itemImage.attr("src", response.findItemsByKeywordsResponse["0"].searchResult["0"].item[i].galleryURL);
 
-                var itemPrice = response.findItemsByKeywordsResponse[0].searchResult[0].item[i].discountPriceInfo[0].originalRetailPrice[0].__value__;
-                var p = $("<div class='card-footer text-center'>").text(itemPrice);
+                var itemPrice = response.findItemsByKeywordsResponse["0"].searchResult["0"].item[i].discountPriceInfo["0"].originalRetailPrice["0"].__value__;
+                var p = $("<div class='card-footer text-center'>").text("$" + itemPrice);
                 
+                var buyButton = $("<button type='button' class='buy-button btn btn-primary'>Buy Now</button>");
+
                 itemDiv.append(h);
                 itemDiv.append(itemImage);
                 itemDiv.append(p);
+                itemDiv.append(buyButton);
 
                 $("#ebay-items-view").prepend(itemDiv);
             }
